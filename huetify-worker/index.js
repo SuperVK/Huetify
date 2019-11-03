38be1efd378bb75382d57732335923b6f8aacf1c
SuperVK <victor.klomp@gmail.com>
2019-11-03 22:04:36 +0100

    started with worker logic

--- huetify-worker/index.js
+++ huetify-worker/index.js
@@ -1,4 +1,7 @@
-const Router = require('./router')
+const CLIENT_ID = '6caaf5d0a58e47868553160955e5b8e4'
+const REDIRECT_URL = '/api/spotifycb'
+
+
 
 /**
  * Example of how router can be used in an application
@@ -7,24 +10,25 @@ addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
 
-function handler(request) {
-    const init = {
-        headers: { 'content-type': 'application/json' },
-    }
-    const body = JSON.stringify({ some: 'json' })
-    return new Response(body, init)
-}
 
 async function handleRequest(request) {
-    const r = new Router()
-    // Replace with the approriate paths and handlers
-    r.get('.*/bar', () => new Response('responding for /bar'))
-    r.get('.*/foo', req => handler(req))
-    r.post('.*/foo.*', req => handler(req))
-    r.get('/demos/router/foo', req => fetch(req)) // return the response from the origin
-
-    r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
-
-    const resp = await r.route(request)
-    return resp
+    let requestURL = new URL(request.url)
+    console.log(requestURL.pathname)
+    switch(requestURL.pathname) {
+        case '/spotifylogin': {
+            let scopes = 'user-read-currently-playing'
+            let url = 'https://accounts.spotify.com/authorize' +
+            '?response_type=code' +
+            '&client_id=' + CLIENT_ID +
+            (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+            '&redirect_uri=' + encodeURIComponent('https://' + requestURL.host + REDIRECT_URL)
+            console.log(url)
+            return Response.redirect(url, 301)
+        }
+    }
+    return new Response('404 Not found', {
+        status: 404
+    })
 }
+
+
