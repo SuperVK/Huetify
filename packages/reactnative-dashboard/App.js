4fb3ea29c520d07c48410da48f11def93c7cc320
SuperVK <victor.klomp@gmail.com>
2019-12-24 12:49:41 +0100

    added reactnative

---
+++ packages/reactnative-dashboard/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app! soop</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: "#fff",
+    backgroundColor: '#212121',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
