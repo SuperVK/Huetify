ec005b5addf8997f6fb65b5fc06c6fa9b2e50e4a
SuperVK <victor.klomp@gmail.com>
2020-01-24 23:12:11 +0100

    add the react-app

---
+++ packages/website/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">Huetify</div>
+      <div className="subtitle">
+        Immerse yourself in your music,<br/>
+        by combining your Philips Hue lamps with Spotify
+      </div>
+      <div className="button download">Download</div>
+    </div>
+  );
+}
+
+export default App;
