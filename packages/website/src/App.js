6a151c7deb9bcb933c91699e523d897e43cae412
SuperVK <victor.klomp@gmail.com>
2020-01-25 00:16:14 +0100

    filled in the website

--- packages/website/src/App.js
+++ packages/website/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import corner from './corner.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="title">Huetify</div>
-      <div className="subtitle">
+      <div id="buffer0"></div>
+      <img src={corner} id="corner0" alt="fancy graphic"></img>
+      <div className="Title">Huetify</div>
+      <img src={corner} id="corner1" alt="fancy graphic"></img>
+      <div id="buffer1"></div>
+      <div className="Subtitle">
         Immerse yourself in your music,<br/>
         by combining your Philips Hue lamps with Spotify
       </div>
-      <div className="button download">Download</div>
+      <div id="buffer2"></div>
+      <a className="DownloadButton" href="Huetify Setup 0.0.3.exe">Download</a>
+      <div className="antivirus">*Click on advanced options and continue when windows defender comes up (I swear it's not a virus)</div>
+      <div id="buffer3"></div>
+
+      <footer className="footer">
+          © 2019 Victor Klomp | Built using <a href="https://developer.spotify.com/">Spotify API</a>, <a href="https://developers.meethue.com/">Hue API</a> and <a href="https://reactjs.org/">ReactJS</a>
+      </footer>
     </div>
   );
 }
