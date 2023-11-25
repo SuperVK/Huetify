a84c739ee79c358f10b2eafcd55608b2bd86970a
Shadey <76909479+Shadyyey@users.noreply.github.com>
2023-11-25 17:43:44 +0100

    Update manager.js

--- packages/manager/src/manager.js
+++ packages/manager/src/manager.js
@@ -1,70 +1,75 @@
 import { EventEmitter } from 'events';
 import Song from './song';
-import Hue from './hue';
+import Hue from './lights';
+import Zengge from './lights';
 import Spotify from './spotify';
 
 export default class Manager extends EventEmitter {
     constructor() {
-        super()
-        this.song = new Song()
+        super();
+        this.song = new Song();
         this.hue = new Hue(this)
-        this.spotify = new Spotify(this)
-        this.isPaused = true
-        this.brightness = 254
+        this.zengge = new Zengge(this);
+        this.spotify = new Spotify(this);
+        this.isPaused = true;
+        this.brightness = 254;
     }
 
     start() {
-        this.isPaused = false
-        this.hue.turnOn()
+        this.isPaused = false;
+        this.zengge.turnOn();
+        this.hue.turnOn();
     }
 
     stop() {
-        this.isPaused = true
-        this.hue.turnOff()
+        this.isPaused = true;
+        this.zengge.turnOff();
+        this.hue.turnOff();
     }
 
     switchLight(id) {
-        this.hue.selectedLight = id
+        this.hue.selectedLight = id;
     }
 
     setSpotifyRefreshToken(token) {
-        if(token == null) {
-            this.spotify.stopPolling()
-            this.spotify.refreshToken = null
+        if (token == null) {
+            this.spotify.stopPolling();
+            this.spotify.refreshToken = null;
             this.spotify.accessToken = {
                 token: null,
-                expiry: null
-            }
+                expiry: null,
+            };
             this.spotify.isReady = false;
-            this.stop()
-            this.emit('update')
+            this.stop();
+            this.emit('update');
         } else {
-            this.spotify.refreshToken = token
-            this.spotify.getAccessToken()
+            this.spotify.refreshToken = token;
+            this.spotify.getAccessToken();
             this.spotify.isReady = true;
-            this.spotify.startPolling()
-            this.emit('update')
+            this.spotify.startPolling();
+            this.emit('update');
         }
     }
 
     setHueToken(token) {
-        if(token == null) {
-            this.hue.token = null
+        if (token == null) {
+            this.hue.token = null;
             this.hue.isReady = false;
-            this.stop()
-            this.emit('update')
+            this.stop();
+            this.emit('update');
         } else {
-            this.hue.token = token
-            this.hue.isReady = true
-            this.emit('update')
+            this.hue.token = token;
+            this.hue.isReady = true;
+            this.emit('update');
         }
     }
 
-    setHueIP(IP) {
-        this.hue.ip = IP
+    setLightIdentifier(identifier) {
+        this.zengge.identifier = identifier;
+        this.hue.ip = identifier;
     }
 
     setBrightness(brightness) {
-        this.brightness = brightness
+        this.brightness = brightness;
     }
 }
